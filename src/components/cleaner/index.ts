0fdd60c0f52765c70144b86cdc66314480f09370
Jiwon Jeong <jiwonjeong414@gmail.com>
2025-06-15 11:03:04 -0700

    Delete Unused Files

--- src/components/cleaner/index.ts
+++ src/components/cleaner/index.ts
@@ -1,5 +1,2 @@
-export { default as DriveCleanerPanel } from './DriveCleanerPanel';
 export { default as DriveCleanerDashboard } from './DriveCleanerDashboard';
-export { default as BatchCleanerUI } from './BatchCleanerUI';
-export { default as CleanerAnalytics } from './CleanerAnalytics';
-export { default as SwipeToCleanUI } from './SwipeToCleanUI'; 
+export { default as SwipeToCleanUI } from './SwipeToCleanUI';
